1b603c6e2c1b2d94efa3e4a37336d9e7e94a0eb8
mestery69 <>
2020-06-06 18:41:48 +0200

    :white_check_mark: add tests

---
+++ tests/test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const PrayerTimes = require('../src/index');
+
+describe('PrayerTimes', () => {
+  const prayerManager = new PrayerTimes()
+  describe('#getYearTimes', () => {
+    it('should return a array of 12 items', () => {
+      let prayer = prayerManager.getYearTimes(2020, [0, 0], 'auto', 'auto', '24h');
+      assert.strictEqual(prayer.length, 12);
+    });
+  });
+  describe('#method', () => {
+    it('should return ISNA', () => {
+      prayerManager.method = "ISNA";
+      assert.strictEqual(prayerManager.method, "ISNA");
+    })
+  });
+});
